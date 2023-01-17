838b6292161e5ec776229ae36da97c387f7adf68
Denis Lazykin <dlazykin856@gmail.com>
2023-01-17 12:16:34 +0300

    Стек вызовов.

---
+++ src/main/java/ru/job4j/chapter03/greet/Greet.java
@@ -0,0 +1,23 @@
+package ru.job4j.chapter03.greet;
+
+public class Greet {
+    private static void greet2(String name) {
+        System.out.println("how are you, " + name + "?");
+    }
+
+    private static void bye() {
+        System.out.println("ok bye!");
+    }
+
+    private static void greet(String name) {
+        System.out.println("hello, " + name + "!");
+        greet2(name);
+        System.out.println("getting ready to say bye...");
+        bye();
+    }
+
+    public static void main(String[] args) {
+        greet("Den");
+        greet("Nasty");
+    }
+}
