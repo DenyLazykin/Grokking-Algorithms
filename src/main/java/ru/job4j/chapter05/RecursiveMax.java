086c3f424ea56f1b676d088ad5932e2c0215e2ba
Denis Lazykin <dlazykin856@gmail.com>
2023-01-18 13:52:59 +0300

    Упражнение 4.3.Найдите наибольшее число в списке.

---
+++ src/main/java/ru/job4j/chapter05/RecursiveMax.java
@@ -0,0 +1,17 @@
+package ru.job4j.chapter05;
+
+import java.util.Arrays;
+
+public class RecursiveMax {
+    private static int max(int[] list) {
+        if (list.length == 2) {
+            return Math.max(list[0], list[1]);
+        }
+        int subMax = max(Arrays.copyOfRange(list, 1, list.length));
+        return Math.max(list[0], subMax);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(max(new int[]{1, 5, 10, 25, 165, 1}));
+    }
+}
