94c17d19509fed282b49488107bc5af0eacfac56
Denis Lazykin <dlazykin856@gmail.com>
2023-01-18 11:02:54 +0300

    Упражнение 4.2.Рекурсивная функция для подсчёта элементов в списке.

---
+++ src/main/java/ru/job4j/chapter05/RecursiveSum.java
@@ -0,0 +1,17 @@
+package ru.job4j.chapter05;
+
+import java.util.Arrays;
+
+public class RecursiveSum {
+    private static int sum(int[] array) {
+        if (array.length == 0) {
+            return 0;
+        } else {
+            return array[0] + sum(Arrays.copyOfRange(array, 1, array.length));
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(new int[]{1, 2, 3, 4}));
+    }
+}
