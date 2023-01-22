c58689da7fbf962bb2d08a486e76d95f41f7df10
Denis Lazykin <dlazykin856@gmail.com>
2023-01-22 13:20:26 +0300

    Хеш таблицы.Цены на продукты питания.

---
+++ src/main/java/ru/job4j/chapter05/PriceOfGroceries.java
@@ -0,0 +1,14 @@
+package ru.job4j.chapter05;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PriceOfGroceries {
+    public static void main(String[] args) {
+        Map<String, Double> book = new HashMap<>();
+        book.put("apple", 0.67);
+        book.put("milk", 1.49);
+        book.put("avocado", 1.49);
+        System.out.println(book);
+    }
+}
