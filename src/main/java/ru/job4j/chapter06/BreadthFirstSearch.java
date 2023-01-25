33409c96e4e63dc2d3cd9a85c0810edf5c35e1e2
Denis Lazykin <dlazykin856@gmail.com>
2023-01-25 10:51:08 +0300

    Поиск в ширину.Добавил java doc.

--- src/main/java/ru/job4j/chapter06/BreadthFirstSearch.java
+++ src/main/java/ru/job4j/chapter06/BreadthFirstSearch.java
@@ -5,6 +5,13 @@ import java.util.*;
 public class BreadthFirstSearch {
     private static Map<String, List<String>> graph = new HashMap<>();
 
+    /**
+     * (17)Список позволяющий отслеживать, каких людей вы искали ранее.
+     * (20)Осуществляем поиск этого человека только в том случае, если вы еще не искали его.
+     * (26)Добавляет пользователя в список, что бы он не проверялся заново.
+     * @param name Имя, которое необходимо найти
+     * @return False, если имя не найдено.
+     */
     private static boolean search(String name) {
         Queue<String> searchQueue = new ArrayDeque<>(graph.get(name));
         List<String> searched = new ArrayList<>();
